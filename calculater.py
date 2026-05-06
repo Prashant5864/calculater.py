ee954f547e7bdd5dc0a0a8b592a396c8776c2785
Prashant5864 <dtrprashant5864sing@gmail.com>
2026-05-06 18:29:25 +0300

    Update calculater.py
    a working calculater in python

--- calculater.py
+++ calculater.py
@@ -4,20 +4,21 @@ num2 = float(input("Enter second number : "))
 operation = input("Enter a operation (+, -, *, /)")
 
 if operation == "+":
-    result = num1+num2
+   result = num1+num2
 
 elif operation == "-":
-    result = num1-num2
+   result = num1-num2
 
 elif operation == "*":
-    result = num1*num2
+   result = num1*num2
 
 elif operation == "/":
-    if num2 != 0:
-        result = "erroe"
-    result = num1/num2
+   if num2 != 0:
+       result = num1/num2
+   else:
+       result = "Error: Division by zero is not allowed."
 
 else:
-    result = "erroe"
+    result = "Error: Invalid operation."
 
-print("result :" , result)
+print("Result :" , result)
